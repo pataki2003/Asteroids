592e0ad520d4b2fd997fb1d0ec4aaa5684799e75
Pataki Attila Bence <patakiattila987@gmail.com>
2025-12-14 22:59:05 +0100

    Adding player and drawing it out on the screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 
@@ -12,14 +13,17 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
     while True:
         log_state()
-        for event in pygame.event.get(0):
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+    
 
 
 if __name__ == "__main__":
