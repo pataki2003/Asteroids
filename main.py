13113d5a09d36a697305d9b6d8ffdb15a5705e11
Pataki Attila Bence <patakiattila987@gmail.com>
2025-12-15 19:18:21 +0100

    Putting the player object into groups

--- main.py
+++ main.py
@@ -7,23 +7,26 @@ from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
     clock = pygame.time.Clock()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-
     while True:
         log_state()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
-        player.update(dt)
-
+            
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
-        pygame.display.flip()
+        for player in drawable:
+            player.draw(screen)
 
+        
+        pygame.display.flip()
         dt = clock.tick(60) / 1000
 
 
